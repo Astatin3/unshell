b6b665dfad911f4a71e5e16b086a8974dab26085
Michael Mikovsky <77305074+Astatin3@users.noreply.github.com>
2025-11-07 09:55:17 -0700

    Update build.sh

--- unshell-module-test/build.sh
+++ unshell-module-test/build.sh
@@ -1,12 +1,6 @@
-# RUSTFLAGS="-Zlocation-detail=none -Zfmt-debug=none" \
-# rustup run nightly cargo build --release
+RUSTFLAGS="-Zlocation-detail=none -Zfmt-debug=none" cargo +nightly build \
+  -Z build-std=std,panic_abort \
+  -Z build-std-features="optimize_for_size" \
+  --release
 
-# rustup run nightly cargo build \
-#     --release \
-#     --no-default-features \
-#     -Zbuild-std="core,std,alloc,proc_macro,panic_abort" \
-#     -Zbuild-std-features="panic_immediate_abort"
-
-
-# RUSTFLAGS="-Z build-std" \
-rustup run nightly cargo build --release
+# upx ./target/release/libunshell_module_test.so
