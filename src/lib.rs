219c7254fb6d70e8cd02223b341296fd1cfe7ab5
Michael Mikovsky <77305074+Astatin3@users.noreply.github.com>
2026-02-20 14:13:41 -0700

    Remove old code

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,10 @@
 #![no_main]
 
-pub mod config;
 mod error;
 pub mod logger;
 pub mod tree;
 
-mod announcement;
-
 pub use error::{ModuleError, Result};
-
-pub use announcement::Announcement;
 
 // Re-exports
 pub use serde_json::{Value, json};
--- ush-server/src/server/blobs.rs
+++
@@ -1,9 +0,0 @@
-use serde_json::Value;
-
-use crate::Server;
-
-impl Server {
-    pub fn get_blobs(&self) -> Result<Vec<Value>, String> {
-        Ok(Vec::new())
-    }
-}
